58b14b090871b74484bc8903212283bfc86259dd
k912867884 <k912867884@sohu.com>
2019-08-02 21:26:36 +0800

    1

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <!-- 页头 -->
+      <div></div>
+      <div></div>
+   </div>
+</template>
+<script>
+export default {
+   data(){
+      return{}
+   }
+}
+</script>
+<style scoped>
+
+</style>
+
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>全世界你最帅</h1>
   </div>
 </template>
